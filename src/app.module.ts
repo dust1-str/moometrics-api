9c2e256df5b8c98865a489dad3f7e53e05d249ac
dust1-str <angel.t069@outlook.com>
2025-09-18 21:26:21 -0600

    Add DB connection and TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
